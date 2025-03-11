44b652eae5a5f5f1b0a1f477440c182ed1a2374f
aluisaoliveira <aluisaoliveira@ipvc.pt>
2025-03-11 19:59:53 +0000

    exp

--- DBLayer/Entities/Experience.cs
+++ DBLayer/Entities/Experience.cs
@@ -8,5 +8,19 @@ public class Experience
     [Key]
     public Guid ExperienceID { get; set; }
     
+    
+    public string Title { get; set; }
+
+    public string CompanyName { get; set; }
+
+    public int StartYear { get; set; }
+
+    public int EndYear { get; set; }
+
+    public Guid ProfileID  { get; set; }
+
+    [ForeignKey("ProfileID")]
+    public TalentProfile TalentProfile { get; set; }
+    
 
 }
