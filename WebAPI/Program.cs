7ed002d8bb8c7f4589e21b6bdedc41955e0b45e1
gabrielaribeiro <gabrielatr200@gmail.com>
2025-03-17 18:40:36 +0000

    Add migration PK

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,13 +1,17 @@
 using ESOF.WebApp.DBLayer.Context;
-using Helpers.Models;
+using ESOF.WebApp.DBLayer.Entities;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// 🔹 Register ApplicationDbContext with Dependency Injection
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -18,6 +22,23 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// ✅ Update route to inject `ApplicationDbContext`
+app.MapPost("/create_account", async (User user, [FromServices] ApplicationDbContext db) =>
+    {
+        var newUser = new User
+        {
+            username = user.username,
+            password = user.password, 
+            fk_role_id = user.fk_role_id
+        };
 
+        db.Users.Add(newUser);
+        await db.SaveChangesAsync();
+
+        return Results.Created($"/users/{newUser.user_id}", newUser);
+    })
+    .WithName("CreateUser")
+    .WithOpenApi();
+
+app.UseHttpsRedirection();
 app.Run();
