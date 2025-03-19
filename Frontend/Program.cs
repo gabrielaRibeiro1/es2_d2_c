0cee48c3ac959ba9ccbea41a8fa00eaa826fd452
aluisaoliveira <aluisaoliveira@ipvc.pt>
2025-03-19 22:08:02 +0000

    n

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -8,15 +8,23 @@ var builder = WebApplication.CreateBuilder(args);
 // Adicionar serviços de autenticação no Blazor
 builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
 builder.Services.AddAuthorizationCore();
-
+builder.Services.AddAntiforgery();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(EnvFileHelper.GetString("API_URL")) });
 builder.Services.AddScoped<ApiHelper>();
+
+// Adiciona os serviços necessários para Razor Components e autorização
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddAuthorization();  // <-- Adicione esta linha
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
+
+app.UseAuthorization(); // <-- Adicione esta linha
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
