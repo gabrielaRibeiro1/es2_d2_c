ed2ee25daebdb1188e79c19de6874a330ea0cd27
aluisaoliveira <aluisaoliveira@ipvc.pt>
2025-03-21 21:24:45 +0000

    Frontend User info

--- Frontend/Helpers/ApiHelper.cs
+++ Frontend/Helpers/ApiHelper.cs
@@ -16,4 +16,19 @@ public class ApiHelper(HttpClient httpClient)
             throw new ApplicationException($"Error fetching data from {url}: {e.Message}");
         }
     }
+    
+    public async Task<bool> PutToApiAsync<T>(string url, T data)
+    {
+        try
+        {
+            var response = await httpClient.PutAsJsonAsync(url, data);
+            return response.IsSuccessStatusCode;
+        }
+        catch (HttpRequestException e)
+        {
+            throw new ApplicationException($"Error updating data at {url}: {e.Message}");
+        }
+    }
+
+
 }
---
+++ Helpers/Models/UserViewModel.cs
@@ -0,0 +1,9 @@
+namespace Helpers.Models
+{
+    public class UserViewModel
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public int RoleId { get; set; }
+    }
+}
