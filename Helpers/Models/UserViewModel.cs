a32b15343397e9d67a773eb2da8be2d4b4c06bfb
aluisaoliveira <aluisaoliveira@ipvc.pt>
2025-04-04 14:35:07 +0100

    Revert "Frontend User info"
    This reverts commit ed2ee25daebdb1188e79c19de6874a330ea0cd27.

--- Helpers/Models/UserViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Helpers.Models
-{
-    public class UserViewModel
-    {
-        public int UserId { get; set; }
-        public string Username { get; set; }
-        public int RoleId { get; set; }
-    }
-}
