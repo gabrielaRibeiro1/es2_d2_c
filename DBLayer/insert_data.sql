758ded6e7a0720c0bfef8ac8f79bd03a9bb730e5
gabrielaribeiro <gabrielatr200@gmail.com>
2025-05-19 22:30:36 +0100

    db

--- DBLayer/insert_data.sql
+++ DBLayer/insert_data.sql
@@ -9,28 +9,28 @@ INSERT INTO es2.public."Roles" (role, role_id) VALUES
 
 -- Inserir Skills
 INSERT INTO es2.public."Skills" (name, area, skill_id) VALUES
-                                              ('JAVA', 'Programming', 4),
-                                              ('ORACLE', 'Database', 5),
-                                              ('Product Owner', 'Management', 6);
+                                              ('JAVA', 'Programming', 1),
+                                              ('ORACLE', 'Database', 2),
+                                              ('Product Owner', 'Management', 3);
 
 -- Inserir UserSkills
 INSERT INTO es2.public."UserSkills" ("UserId", "SkillId") VALUES
-                                             (4, 1),
-                                             (4, 2),
-                                             (4, 3);
+                                             (2, 1),
+                                             (2, 2),
+                                             (2, 3);
 
 -- Inserir TalentProfiles
 INSERT INTO es2.public."TalentProfiles"  (profile_name, country, email, price, privacy, category, fk_user_id)
-VALUES ('John Doe', 'Brazil', 'john@example.com', 50.0, 0, 'Backend', 4);
+VALUES ('John', 'Brazil', 'john@example.com', 50.0, 0, 'Backend', 2);
 
 INSERT INTO es2.public."TalentProfileSkills" ("TalentProfileId", "SkillId", "YearsOfExperience")
 VALUES
-    (4, 1, 3),
-    (4, 2, 5);
+    (1, 1, 3),
+    (1, 2, 5);
 -- Inserir Experiences
 INSERT INTO es2.public."Experiences" (company_name, start_year, end_year, fk_profile_id, experience_id) VALUES
-                                                                                               ('Tech Corp', '2018', '2021', 4, 1),
-                                                                                               ('Data Solutions', '2021', 'Present', 4, 2);
+                                                                                               ('Tech Corp', '2018', '2021', 1, 1),
+                                                                                               ('Data Solutions', '2021', '0000', 1, 2);
 
 -- Inserir WorkProposals
 INSERT INTO es2.public."WorkProposals" (proposal_name, category, necessary_skills, years_of_experience, description, total_hours, fk_user_id, proposal_id) VALUES
