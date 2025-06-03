560feadd07931184900bf90f7763b5d63076540c
FilipeFerreira30 <f.ferreira@ipvc.pt>
2025-06-03 14:21:21 +0100

    fix - missing files

---
+++ Frontend/Helpers/UserSession.cs
@@ -0,0 +1,15 @@
+﻿namespace Frontend.Helpers;
+
+public static class UserSession
+{
+    public static int? UserId { get; set; }
+    public static string? Username { get; set; }
+    public static int? RoleId { get; set; }
+
+    public static void Clear()
+    {
+        UserId = null;
+        Username = null;
+        RoleId = null;
+    }
+}
