efc12c614c47c2ab85bd0b928352af290af3d30b
Pedro1Alves <109022442+Pedro1Alves@users.noreply.github.com>
2025-06-10 13:41:33 +0100

    BLL

---
+++ ESOF.WebApp.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace ESOF.WebApp.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
